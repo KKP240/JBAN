f7fac247354475cd1291d9afb2ac27f7a70a2e07
KKP240 <66070240@kmitl.ac.th>
2025-01-30 17:13:36 +0700

    authMiddleware add aiuthentication

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -12,8 +12,12 @@
 const express = require('express');
 const { registerUser } = require('../controllers/authController');
 const { loginUser } = require('../controllers/authController');
+const authMiddleware = require('../middlewares/authMiddleware');
 
 const router = express.Router();
+router.get('/profile', authMiddleware, (req, res) => {
+    res.json({ message: "Welcome to your profile!", user: req.user });
+});
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 
