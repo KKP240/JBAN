9cd371f6375e470e1fd548deb9c532260a3d9e34
Stacla3 <141387173+Stacla3@users.noreply.github.com>
2025-02-12 01:45:14 +0700

    check eror login

--- src/views/login/script.js
+++ src/views/login/script.js
@@ -1,24 +1,50 @@
-document
-  .querySelector("form")
-  .addEventListener("submit", async function (event) {
-    event.preventDefault();
+///////////////////////////////////////////////////////
 
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
+// Selecting Elements
+const formEl = document.querySelector("form");
 
-    const response = await fetch("http://localhost:5000/api/auth/login", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ email, password }),
-    });
+///////////////////////////////////////////////////////
 
-    const data = await response.json();
+// Check form data
+const processForm = async function (e) {
+  e.preventDefault();
 
-    if (response.ok) {
-      alert("เข้าสู่ระบบ!");
-      localStorage.setItem("token", data.token); // เก็บ token ใน localStorage
-      window.location.href = "/dashboard.html"; // เปลี่ยนหน้า
-    } else {
-      alert("Error: " + data.message);
-    }
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  const response = await fetch("http://localhost:5000/api/auth/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ email, password }),
   });
+
+  const data = await response.json();
+
+  if (response.ok) {
+    alert("เข้าสู่ระบบ!");
+    localStorage.setItem("token", data.token); // เก็บ token ใน localStorage
+    window.location.href = "/index.html"; // เปลี่ยนหน้า
+  }
+
+  if (!response.ok) {
+    document.querySelectorAll(".eror").forEach((e) => (e.innerHTML = "&nbsp;"));
+
+    if (data.message === "This email is not registered") {
+      const curEl =
+        document.querySelector(".input-email").parentNode.nextElementSibling;
+      curEl.textContent = data.message;
+    }
+    if (data.message === "Invalid email or password") {
+      const curEl =
+        document.querySelector(".input-password").parentNode.nextElementSibling;
+      curEl.textContent = data.message;
+    }
+  }
+};
+
+///////////////////////////////////////////////////////
+
+// Add event listener
+formEl.addEventListener("submit", processForm);
+
+///////////////////////////////////////////////////////
