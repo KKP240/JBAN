26b6712fd17e8456a912b11ffc3dde055602d43c
KKP240 <66070240@kmitl.ac.th>
2025-02-15 15:44:27 +0700

    api me current-user fixed

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -12,6 +12,7 @@
 const express = require('express');
 const { registerUser } = require('../controllers/authController');
 const { loginUser } = require('../controllers/authController');
+const { me } = require('../controllers/authController');
 const authMiddleware = require('../middlewares/authMiddleware');
 
 const router = express.Router();
